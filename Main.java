f49ed9f842fc06cbe2992b39a760239fd82ac418
HOU maxime <wtfomg@live.fr>
2021-12-28 13:28:10 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        Julia j = new Julia();
-        System.out.println(j);
-        int div = j.divergenceIndex(j.getZ0());
-        System.out.println(div);
-    }
-}
