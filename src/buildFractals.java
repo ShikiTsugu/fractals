4e1b2ceb32861986e966ba2eef3112e405c2aa41
hou <wtfomg@live.fr>
2021-12-28 13:55:56 +0100

    edit

--- src/buildFractals.java
+++ src/buildFractals.java
@@ -16,29 +16,22 @@ public class buildFractals {
 
     }
 
-    public void buildPicture(){
-        var img=new BufferedImage(210, 210, BufferedImage.TYPE_INT_RGB);
+    public void buildPicture() {
+        var img = new BufferedImage(210, 210, BufferedImage.TYPE_INT_RGB);
         int id = 1;
         File f = new File("Fractal.png");
-        if(f.exists()){
-            f = new File("Fractal"+id+".png");
-            while(f.exists()){
+        if (f.exists()) {
+            f = new File("Fractal" + id + ".png");
+            while (f.exists()) {
                 id++;
-                f = new File("Fractal"+id+".png");
+                f = new File("Fractal" + id + ".png");
             }
-            try {
-                ImageIO.write(img, "PNG", f);
-            }catch(IOException e){
-                System.out.println("Couldn't write in file");
-                e.printStackTrace();
-            }
-        }else{
-            try {
-                ImageIO.write(img, "PNG", f);
-            }catch(IOException e){
-                System.out.println("Couldn't write in file");
-                e.printStackTrace();
-            }
+        }
+        try {
+            ImageIO.write(img, "PNG", f);
+        } catch (IOException e) {
+            System.out.println("Couldn't write in file");
+            e.printStackTrace();
         }
     }
 }
