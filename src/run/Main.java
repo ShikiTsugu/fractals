ed1fc00bad4f669a1c181dee851290dfa5ef6154
hou <wtfomg@live.fr>
2021-12-30 18:38:05 +0100

    ajout initialisation requetes

--- src/run/Main.java
+++ src/run/Main.java
@@ -23,6 +23,7 @@ public class Main {
         }
         System.out.println(set);
         buildFractals f = new buildFractals(set);
+        f.requestSettings();
         f.buildPicture();
     }
 }
